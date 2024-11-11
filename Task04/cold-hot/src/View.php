8289cdf9c3deaeb2a5c170dacc7fa6881e84547b
Mar1o2003 <shokov.cosmo@mail.ru>
2024-11-11 21:12:23 +0300

    Task04

---
+++ Task04/cold-hot/src/View.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mario2003\ColdHot\View;
+
+use cli; // Подключение функционала wp-cli/php-cli-tools
+
+function showStartScreen() {
+    cli\line("Welcome to Cold-hot!");
+}
+
+function getUserInput($message = 'Enter your guess') {
+    return cli\prompt($message);
+}
+
+function showFeedback($feedback) {
+    cli\line($feedback);
+}
+
+function showGameReplay(array $game, array $moves) {
+    cli\line("Game ID: {$game['id']}");
+    cli\line("Player: {$game['player_name']}");
+    cli\line("Field size: {$game['field_size']}");
+    cli\line("Target number: {$game['target_number']}");
+    cli\line("Start time: {$game['start_time']}");
+    cli\line("End time: {$game['end_time']}");
+    cli\line("Attempts: {$game['attempts']}");
+    cli\line("Result: {$game['result']}");
+    cli\line("\nMoves:");
+    foreach ($moves as $move) {
+        cli\line("Move {$move['move_number']}: Guess {$move['guess']}, Feedback {$move['feedback']}, Time {$move['time']}");
+    }
+}
